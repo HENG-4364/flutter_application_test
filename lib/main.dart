bc3d1a1f009ce992565f97e87d81f032ffbb07cf
Lyheng <senglyheng2001@gmail.com>
2023-12-27 17:01:00 +0700

    add-row

--- lib/main.dart
+++ lib/main.dart
@@ -25,48 +25,34 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Demo App Flutter"),
-      ),
-      // body: Center(
-      //   child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      //     const Text("You can Click button any items"),
-      //     Text(
-      //       number.toString(),
-      //       style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-      //     )
-      //   ]),
-      // ),
-      body: Container(
-        constraints: BoxConstraints(maxWidth: 200, maxHeight: 300),
-        margin: EdgeInsets.only(left: 105, top: 50),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            // borderRadius: BorderRadius.circular(10),
-            // color: Color(0xFF96bbbb),
-            gradient: LinearGradient(colors: [Color(0xFF96bbbb), Colors.blue ,Colors.red] ,begin: Alignment.topCenter,end: Alignment.bottomCenter),
-            boxShadow: const [
-              BoxShadow(color: Colors.purple, blurRadius: 6, spreadRadius: 4)
-            ]),
-        padding: const EdgeInsets.all(18),
-        child: const Text("Flutter",
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold)),
-        transform: Matrix4.rotationZ(0.5),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            number++;
-          });
-          print(number);
-        },
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Demo App Flutter"),
+        ),
+        body: Container(
+          height: 200,
+          color: Colors.blueGrey,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              )
+            ],
+          ),
+        ));
   }
 }
