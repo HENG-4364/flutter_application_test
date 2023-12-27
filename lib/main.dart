126a8e597ad7de2fc67c01bd619e48fbc835430b
Lyheng <senglyheng2001@gmail.com>
2023-12-27 17:46:31 +0700

    create-card

--- lib/main.dart
+++ lib/main.dart
@@ -29,22 +29,70 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("Demo App Flutter"),
         ),
-        body: Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.green,
-              )
-            ],
+        body:  Container(     
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, top: 10),
+                      child: Text(
+                        "STUDENT",
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, top: 1),
+                  child: Row(
+                    children: [
+                      Text(
+                        "INDENTITY CARD",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("Studies at"),
+                          Text("INERATIONAL UNIVERSITY",style: TextStyle(fontWeight: FontWeight.bold),),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("Name"),
+                          Text("Calvin Jameson",style: TextStyle(fontWeight: FontWeight.bold)),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("Born"),
+                          Text("05/12/2001",style: TextStyle(fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Image.network("https://static.vecteezy.com/system/resources/thumbnails/024/095/208/small/happy-young-man-smiling-free-png.png")
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
